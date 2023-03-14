510d9d8099aaf159a26bba5fa54c9410487f0c3a
romanssam <troubledyouthh@outlook.com>
2023-03-14 20:42:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <span>scoped</span>
+    </div>
+  )
+}
+
+export default App
